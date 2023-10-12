3c65de2af6208938178a32562ea0f6a28cdbbee9
Julia Justino <stephany.justino@sptech.school>
2023-10-12 01:41:02 -0300

    dataframe

---
+++ script/data15.R
@@ -0,0 +1,14 @@
+seed = 20
+vendores <- c("Ana", "Flavia", "Pedro", "Mariana")
+produtos <- c("calçaFlaire", "calçaSkinny", "vestido", "blusa", "manta", "saia", "casaco", "meia")
+numeros <- seq(5,0)
+quantidade <- sample(numeros, 6)
+vend <- rep(vendores, 8)
+dias <- rep(1:30, 4)
+#vendas <- data.frame(dias,vend, amostra)
+indice <- c(1,2)
+
+vendas<- data.frame(vend)
+vendas<- vendas[-indice,]
+
+geral<- data.frame(vendas,dias, produtos, quantidade)

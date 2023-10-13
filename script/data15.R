ed405f855570781ea2b5b44a5b48102494db28b2
Julia Justino <stephany.justino@sptech.school>
2023-10-12 21:13:23 -0300

    aleatorio

--- script/data15.R
+++ script/data15.R
@@ -2,17 +2,21 @@ seed = 20
 vendores <- c("Ana", "Flavia", "Pedro", "Mariana")
 produtos <- c("calçaFlaire", "calçaSkinny", "vestido", "blusa", "manta", "saia", "casaco", "meia")
 numeros <- seq(5,0)
-quantidade <- sample(numeros, 6)
-vend <- rep(vendores, 8)
-dias <- rep(1:30, 4)
+quantidade <- sample(numeros, size=32, replace=TRUE)
+vend <- sample(vendores,size=32, replace=TRUE)
+produ <- sample(produtos, size=32, replace = TRUE)
+dias <- rep(1:32, 1)
 #vendas <- data.frame(dias,vend, amostra)
-indice <- c(1,2)
+indice <- c(1)
 
-vendas<- data.frame(vend)
-vendas<- vendas[-indice,]
+vendas<- data.frame(Vendedor = vend)
+produtosV <- data.frame(Produtos = produ)
 
-geral<- data.frame(vendas,dias, produtos, quantidade)
+
+geral<- data.frame(vendas,dias, produtosV, quantidade)
 
 proc <- quadro2$Preco.unitario * 0.5
 
-quadro2["aumento"] <- c(proc)
+quadro2["Aumento"] <- c(proc)
+
+merda<-sample(vendores,size=100, replace=TRUE)

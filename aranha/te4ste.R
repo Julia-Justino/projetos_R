03626e745f71e088a611db0cdd490d01bc3574da
Julia Justino <stephany.justino@sptech.school>
2023-10-15 23:26:43 -0300

    para finalizar

--- aranha/te4ste.R
+++ aranha/te4ste.R
@@ -7,14 +7,15 @@ library(ggpubr)
 
 dfTemp <- data.frame(Dia = climaTempo$Data ,
                      "Temperatura mínima" = climaTempo$temperaturaMinima, 
-                     "Temperatura máxima" = climaTempo$temperaturaMaxima
+                     "Temperatura máxima" = climaTempo$temperaturaMaxima,
+                     "umidade" = climaTempo$uminidadeRelativaPorcen
                      )
 # Convertir a coluna Dia para formato de data
 dfTemp$Dia <- as.Date(dfTemp$Dia, format = "%d/%m/%Y")
 
 mediaDiaClima <- dfTemp %>%
   group_by(Dia) %>%
-  summarise(mediaTempMin = mean(Temperatura.mínima), mediaTempMax = mean(Temperatura.máxima))
+  summarise(mediaTempMin = mean(Temperatura.mínima), mediaTempMax = mean(Temperatura.máxima), umidade = mean(umidade))
 
 
 mediaDiaClima$Dia <- as.Date(mediaDiaClima$Dia, format = "%d/%m/%Y")
@@ -22,8 +23,8 @@ dadosDia <- mediaDiaClima[1:31, ]
 dadosFeveire <- mediaDiaClima[32:59, ]
 dadosMarco <- mediaDiaClima[60:90, ]
 dadosAbril <- mediaDiaClima[91:120, ]
-dadosMaio <- mediaDiaClima[]
-#=========================Janeiro Temperatura==========================================================
+
+#=====================================Janeiro==========================================================
 
 ggMAX <- ggplot(dadosDia, aes(x = Dia, y = mediaTempMax, group=1, label=round(mediaTempMax))) +
   geom_line(stat = "identity", color="red", size=1) +
@@ -40,8 +41,16 @@ ggMIN <- ggplot(dadosDia, aes(x = Dia, y = mediaTempMin, group=1, label=round(me
   labs(x = "Data", y = "Temperatura mínima", title = "Gráfico mensal de temperaturas ºC mínima (Janeiro)")
  
 ggMAX + ggMIN + plot_layout(nrow = 2)
+
+ggUMI<- ggplot(dadosDia, aes(x = Dia, y = umidade, group=1, label=round(umidade), color = umidade)) +
+  geom_bar(stat = "identity", size=1) +
+  geom_point()+
+  geom_text(nudge_y = 1.7)+
+  labs(x = "Data", y = "Umidade mínima", title = "Gráfico mensal de umidade (Janeiro)")
 #=======================================================================================================
 
 
-
-
+#================================Todo===================================================================
+umidadeAno <- hist(mediaDiaClima$umidade,  col="#abf5bf",main="Umidade anual 2023")
+tempMaxAno <- hist(mediaDiaClima$mediaTempMax, col="#52d977", main="Temperatura máxima anual 2023")     
+tempMinAno<-hist(mediaDiaClima$mediaTempMin, col="#669172", main = "Temperatura mínima anual 2023")

4b185f7a7c00e957dc27fc48b477e86e8d40f084
Julia Justino <stephany.justino@sptech.school>
2023-12-09 14:52:21 -0300

    Add files via upload

--- nuvem/nuvem/erros.R
+++ nuvem/nuvem/erros.R
@@ -1,9 +1,13 @@
 # Crie um vetor de nomes de chamados
-chamados <- c("dashboard_não_aparece", "dashboard_travado", "temperatura_elevada", "CPU_consumo_elevado", "aplicação_lenta", "upload_lento", "download_lento", "memoria_cheia", "alerta_dispara", "dados_incorretos", "disco_consumo_elevado", "usuario_não_pode_ser_cadastrado", "maquina_não_pode_ser_cadastrada", "login_não_funciona")
+chamados <- c("dashboard_não_aparece", "dashboard_travado", "temperatura_elevada",
+              "CPU_consumo_elevado", "aplicação_lenta", "upload_lento", "download_lento", 
+              "memoria_cheia", "alerta_dispara", "dados_incorretos", "disco_consumo_elevado", 
+              "usuario_não_pode_ser_cadastrado", "maquina_não_pode_ser_cadastrada",
+              "login_não_funciona")
+
 chamadosDf<-data.frame(chamados)
 
 dns<-sample(c(chamadosDf$chamados[1], chamadosDf$chamados[6]))
-
 vinte<-sample(c(chamadosDf$chamados[7], chamadosDf$chamados[8], chamadosDf$chamados[9], 
                 chamadosDf$chamados[10], chamadosDf$chamados[11], chamadosDf$chamados[13], chamadosDf$chamados[14]))
 dt<-sample(chamadosDf$chamados[2])
@@ -21,4 +25,27 @@ al<-rep(al,100)
 ca<-rep(ca,40)
 
 
-length(ca)
+total <-sample(c(vinte, dns_re, dt, te, cpuCe, al, ca))
+dados<-sample(total, 300)
+
+
+#install.packages(c("wordcloud","RColorBrewer", "wordcloud2"))
+
+library(wordcloud)
+library(RColorBrewer)
+library(wordcloud2)
+library(tm)
+
+docs <-Corpus(VectorSource(dados))
+dtm <- TermDocumentMatrix(docs)
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix),decreasing=TRUE)
+df <- data.frame(word = names (words) ,freq=words)
+View(df)
+
+wordcloud(words = df$word, freq = df$freq, min.freq = 1,
+          max.words=100, randow.order = FALSE, rot.per=0.35,
+          colors=brewer.pal(8,"Dark2"), scale = c(2,1))
+
+wordcloud2(data = df, size = 0.5, color = 'random-light',
+           backgroundColor = 'black', rotateRatio = 0, shape = 'circle')
